8931a188af2d76340b1f61629226ed26d4a0bae8
Sadrac Tijerina <reavity3@gmail.com>
2022-08-02 22:19:46 -0400

    Initial commit

---
+++ app/src/main/java/sadrac/tijerina/myfirstapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package sadrac.tijerina.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val myTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+
+        btnClickMe.setOnClickListener {
+            timesClicked = timesClicked + 1
+
+            myTextView.text = timesClicked.toString()
+
+            Toast.makeText(this, "Hey I am testing", Toast.LENGTH_LONG).show()
+        }
+    }
+}
